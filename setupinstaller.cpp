5590f8dc04f75f511c0716b691e4a4582ffe912f
Noisy_Bomb <vladdimirylanov98@gmail.com>
2026-02-23 21:28:53 +0300

    SetupInstaller

---
+++ setupinstaller.cpp
@@ -0,0 +1,3 @@
+#include "setupinstaller.h"
+
+setupinstaller::setupinstaller() {}
---
+++ setupinstaller.h
@@ -0,0 +1,10 @@
+#ifndef SETUPINSTALLER_H
+#define SETUPINSTALLER_H
+
+class setupinstaller
+{
+public:
+    setupinstaller();
+};
+
+#endif // SETUPINSTALLER_H
